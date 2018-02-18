9dd2e428105994de9feca06c5071ecaf4847ea86
TheRealPear <pearofficialyt@gmail.com>
2018-02-18 11:38:00 -0500

    a
    a

--- handlers.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
-
-client.on("message", async msg => {
-    if(msg.mentions.everyone === true){
-        msg.delete()
-        msg.channel.send("Pinging the entire server is not allowed!")
-    }
-})
